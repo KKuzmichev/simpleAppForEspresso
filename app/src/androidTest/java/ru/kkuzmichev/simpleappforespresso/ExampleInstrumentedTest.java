b8437011b891a2f26691ec400e431d46888e5a2f
spisladko <inessaivanovastr@inbox.ru>
2022-03-13 16:31:54 +0300

    second lab

--- app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/ExampleInstrumentedTest.java
@@ -2,20 +2,25 @@ package ru.kkuzmichev.simpleappforespresso;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.action.ViewActions.scrollTo;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.contrib.DrawerMatchers.isOpen;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasAction;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasData;
+import static androidx.test.espresso.matcher.ViewMatchers.isAssignableFrom;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withParent;
 
 import android.content.Context;
-import android.view.View;
+import android.content.Intent;
+import android.view.ActionMode;
 
-import androidx.test.core.app.ActivityScenario;
-import androidx.test.espresso.action.ViewActions;
+import androidx.appcompat.widget.ActionMenuView;
+import androidx.test.espresso.ViewInteraction;
 import androidx.test.espresso.contrib.DrawerActions;
 import androidx.test.espresso.contrib.NavigationViewActions;
-import androidx.test.espresso.matcher.ViewMatchers;
+import androidx.test.espresso.intent.Intents;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
@@ -24,6 +29,7 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static org.hamcrest.Matchers.allOf;
 import static org.junit.Assert.*;
 
 /**
@@ -35,28 +41,27 @@ import static org.junit.Assert.*;
 public class ExampleInstrumentedTest {
 
     @Rule
-    public ActivityScenarioRule rule = new ActivityScenarioRule<MainActivity>(MainActivity.class);
+    public ActivityScenarioRule rule = new ActivityScenarioRule<>(MainActivity.class);
 
     @Test
     public void useAppContext() {
-        // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("ru.kkuzmichev.simpleappforespresso", appContext.getPackageName());
     }
     @Test
     public void testNavigationToGallery() {
 
-        onView(ViewMatchers.withId(R.id.drawer_layout)).perform(DrawerActions.open());
-        onView(ViewMatchers.withId(R.id.drawer_layout)).check(matches(isOpen()));
-        onView(ViewMatchers.withId(R.id.nav_view)).perform(NavigationViewActions.navigateTo(R.id.nav_gallery));
+        onView(withId(R.id.drawer_layout)).perform(DrawerActions.open());
+        onView(withId(R.id.drawer_layout)).check(matches(isOpen()));
+        onView(withId(R.id.nav_view)).perform(NavigationViewActions.navigateTo(R.id.nav_gallery));
 
     }
 
     @Test
     public void testFloatingButton() {
 
-        onView(ViewMatchers.withId(R.id.fab)).perform(click());
-        onView(ViewMatchers.withId(R.id.nav_slideshow)).check(matches(isDisplayed()));
+        onView(withId(R.id.fab)).perform(click());
+        onView(withId(R.id.nav_slideshow)).check(matches(isDisplayed()));
 
     }
 }
