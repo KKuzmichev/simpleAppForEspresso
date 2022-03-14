ef6ccaf68d6b7cfae1d4ad34c54abcee9f64a0f3
spisladko <inessaivanovastr@inbox.ru>
2022-03-14 20:45:58 +0300

    last lab

--- app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/ExampleInstrumentedTest.java
@@ -32,12 +32,14 @@ import org.junit.runner.RunWith;
 import static org.hamcrest.Matchers.allOf;
 import static org.junit.Assert.*;
 
+import io.qameta.allure.android.runners.AllureAndroidJUnit4;
+
 /**
  * Instrumented test, which will execute on an Android device.
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-@RunWith(AndroidJUnit4.class)
+@RunWith(AllureAndroidJUnit4.class)
 public class ExampleInstrumentedTest {
 
     @Rule
--- app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/SecondLabTests.java
+++ app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/SecondLabTests.java
@@ -10,24 +10,39 @@ import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
 
+import android.Manifest;
 import android.content.Intent;
+import android.os.Environment;
 
 import androidx.test.espresso.IdlingRegistry;
 import androidx.test.espresso.ViewInteraction;
 import androidx.test.espresso.intent.Intents;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
+import androidx.test.rule.GrantPermissionRule;
+import androidx.test.uiautomator.UiDevice;
+
+import io.qameta.allure.android.runners.AllureAndroidJUnit4;
+import io.qameta.allure.kotlin.Allure;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TestWatcher;
 import org.junit.runner.RunWith;
 
 import static org.hamcrest.CoreMatchers.allOf;
 
-@RunWith(AndroidJUnit4.class)
+import io.qameta.allure.android.runners.AllureAndroidJUnit4;
+
+@RunWith(AllureAndroidJUnit4.class)
 public class SecondLabTests {
 
     @Before
@@ -38,6 +53,23 @@ public class SecondLabTests {
     @Rule
     public ActivityTestRule<MainActivity> activityActivityTestRule = new ActivityTestRule<>(MainActivity.class);
 
+    @Rule
+    public GrantPermissionRule mRuntimePermissionRule = GrantPermissionRule
+            .grant(
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+            );
+
+    @Rule
+    public TestWatcher watcher = new TestWatcher() {
+        @Override
+        protected void failed(Throwable e, org.junit.runner.Description description) {
+            String className = description.getClassName();
+            className = className.substring(className.lastIndexOf('.') + 1);
+            String methodName = description.getMethodName();
+            takeScreenshot(className + "#" + methodName);
+        }
+    };
 
     @Test
     public void checkIntent() {
@@ -79,5 +111,21 @@ public class SecondLabTests {
     @After
     public void unregisterIdlingResources() {
         IdlingRegistry.getInstance().unregister(IdlingResources.idlingResource);
+    }
+
+    private void takeScreenshot(String name) {
+        File path = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "/screenshots/");
+        if (!path.exists()) {
+            path.mkdirs();
+        }
+
+        UiDevice device = UiDevice.getInstance(getInstrumentation());
+        String filename = name + ".png";
+        device.takeScreenshot(new File(path, filename));
+        try {
+            Allure.attachment(filename, new FileInputStream(new File(path, filename)));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
